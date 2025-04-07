68a05cafcdbe15e040a155ffd3ffb3569b4f6cd3
donp8on <paytonadonis@gmail.com>
2025-04-07 16:38:49 -0400

    Feat: Implemented insert function for DynamicLinkedList

--- src/dynamic_list.rs
+++ src/dynamic_list.rs
@@ -12,4 +12,25 @@ impl<T: PartialEq + Clone> DynamicLinkedList<T> {
         DynamicList { head: None }
     }
 
+    pub fn insert(&mut self, data: T) {
+        let new_node = Box::new(Node {
+            data,
+            next: self.head.take(),
+        });
+        match self.head.as_mut() {
+            None => self.head = Some(new_node),
+            Some(mut node) => {
+                while let Some(next) = node.next.as_mut() {
+                    if next.data == new_node.data {
+                        return; // Duplicate found, do not insert
+                    }
+                    node = next;
+                }
+                node.next = Some(new_node);
+            }
+        }
+    }
+
+    
+
 }
