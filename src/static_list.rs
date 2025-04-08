6bea4a74f22b4a8a5f9c1a980cbaa31971759a2e
donp8on <paytonadonis@gmail.com>
2025-04-07 22:04:23 -0400

    Feat: Implement new() constructor with free list initialization

--- src/static_list.rs
+++ src/static_list.rs
@@ -8,3 +8,22 @@ pub struct StaticLinkedList<T, const N: usize> {
     head: Option<usize>,
     free: Option<usize>,
 }
+
+impl<T, const N: usize> StaticLinkedList<T, N> {
+    pub fn new() -> Self {
+        let mut nodes: [StaticNode<T>; N] = std::array::from_fn(|i| StaticNode {
+            data: None,
+            next: Some(i + 1),
+        });
+
+        nodes[N - 1].next = None; // End of free list
+
+        Self {
+            nodes,
+            head: None,
+            free: Some(0),
+        }
+    }
+    
+}
+
