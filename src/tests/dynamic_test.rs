05054d256ebc6e3eb95b75f044f8d3d316721944
donp8on <paytonadonis@gmail.com>
2025-04-07 21:13:39 -0400

    Test: created test for insert_at_index() method

--- src/tests/dynamic_test.rs
+++ src/tests/dynamic_test.rs
@@ -22,4 +22,21 @@ mod dynamic_tests {
         assert_eq!(list.get(2), Some(30));
         assert_eq!(list.get(3), None); // Out of bounds
     }
+
+    #[test]
+    fn test_insert_at_index() {
+        let mut list = DynamicLinkedList::new();
+        list.insert(1);          // [1]
+        list.insert(3);          // [1, 3]
+        list.insert_at_index(1, 2);
+        list.insert_at_index(0, 0);
+        list.insert_at_index(10, 99);
+
+        assert_eq!(list.get(0), Some(0));
+        assert_eq!(list.get(1), Some(1));
+        assert_eq!(list.get(2), Some(2));
+        assert_eq!(list.get(3), Some(3));
+        assert_eq!(list.get(4), None); // out of bounds (99 was not inserted)
+    }
+
 }
